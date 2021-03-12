392691d14d299543049e33bf2fa80fbba0954222
Henadzi <RudchenkoHenadzi@users.noreply.github.com>
2021-03-12 17:46:37 +0300

    feat: result task: typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  function minSortArr(a, b) {
+    return a - b;
+  }
+
+  if(array === undefined || array.length === 0) {
+    return 0;
+  } else {
+      let minValue = array.sort(minSortArr);
+      return array[0];
+  }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  function maxSortArr(a, b) {
+    return a - b;
+  }
+
+  if(array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let maxValue = array.sort(maxSortArr);
+    return array[array.length -1];
+  }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sumArr = 0;
+
+    if(array === undefined || array.length === 0) {
+      return 0;
+    } else {
+
+    for(let i = 0; i < array.length; i++) {
+        sumArr = sumArr + array[i];
+    }
+
+    return sumArr / array.length;
+    }
 }
